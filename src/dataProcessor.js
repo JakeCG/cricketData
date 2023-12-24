aee99ed440c585908b3dbce285733a603d88f127
Jake Gowler <jakecgowler@gmail.com>
2023-12-24 16:40:06 +0000

    upgrading code

--- src/dataProcessor.js
+++ src/dataProcessor.js
@@ -1,35 +1,36 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 
 module.exports = {
   async getTourLists(filePath, gender, countryName) {
-    // Gets a list of data from the filePath, allows user to specify gender, and country, but not essential, to check
-    // for specific teams fixtures.
-    const rawData = fs.readFileSync(filePath, "utf-8");
-    const data = JSON.parse(rawData);
-
     try {
+      const rawData = await fs.readFile(filePath, "utf-8");
+      const data = JSON.parse(rawData);
+
       if (!Array.isArray(data)) {
         console.warn("Data passed is not an array, returning an empty array.");
         return [];
       }
 
+      const isTourOf = (item) => {
+        const lowerName = item.name.toLowerCase();
+        const lowerCountryName = countryName.toLowerCase();
+        return (
+            lowerName.substring(0, 15).includes(lowerCountryName) &&
+            lowerName.includes("tour of")
+        );
+      };
+
       switch (gender) {
         case "male":
           return data.filter(
-            (item) =>
-              item.name.substring(0, 15).includes(countryName) &&
-              item.name.includes("tour of") &&
-              !item.name.includes("Women"),
+              (item) => isTourOf(item) && !item.name.toLowerCase().includes("women")
           );
         case "female":
           return data.filter(
-            (item) =>
-              item.name.substring(0, 15).includes(countryName) &&
-              item.name.includes("Women") &&
-              item.name.includes("tour of"),
+              (item) => isTourOf(item) && item.name.toLowerCase().includes("women")
           );
         default:
-          return data.filter((item) => item.name.includes("tour of"));
+          return data.filter((item) => isTourOf(item));
       }
     } catch (error) {
       console.error("Error processing data:", error);
