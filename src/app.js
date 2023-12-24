8adfff6f216fedc21bfdb6e30d7b77958cf8a98e
Jake Gowler <jakecgowler@gmail.com>
2023-12-24 15:58:30 +0000

    Processing international data based on gender

--- src/app.js
+++ src/app.js
@@ -1,22 +1,54 @@
 const fetchData = require("./fetchData.js");
 const fileHandler = require("./fileHandler.js");
+const dataProcessor = require("./dataProcessor.js");
 
 async function processDataToFile() {
   try {
     const matchList = await fetchData.fetchMatchList();
     await fileHandler.saveDataToJson(matchList);
-
-    // Assuming you want to return a success message
-    return 'Process completed successfully';
+    return "Process completed successfully";
   } catch (err) {
     console.log(err);
-    throw err; // Rethrow the error to be caught in the 'catch' block below
+    throw err;
   }
 }
 
-processDataToFile()
+async function printInternationalGames(gender) {
+  try {
+    return await dataProcessor.getTourLists("testData.json", gender);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+// processDataToFile()
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log("An error occurred: ", error);
+//   });
+
+printInternationalGames("male")
     .then((result) => {
-      console.log(result); // This will log 'Process completed successfully' on success
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log("An error occurred: ", error);
+    });
+
+printInternationalGames("female")
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log("An error occurred: ", error);
+    });
+
+printInternationalGames(null)
+    .then((result) => {
+      console.log(result);
     })
     .catch((error) => {
       console.log("An error occurred: ", error);
---
+++ src/dataProcessor.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+module.exports = {
+  async getTourLists(filePath, gender) {
+    const rawData = fs.readFileSync(filePath, 'utf-8');
+    const data = JSON.parse(rawData);
+    if (!Array.isArray(data)) {
+        console.warn('Data passed is not an array, returning an empty array.')
+        return [];
+    }
+
+    if (gender === 'male') {
+        return data.filter((item) => item.name.includes("tour of") && !item.name.includes("Women"));
+    } else if (gender === 'female') {
+        return data.filter((item) => item.name.includes("Women") && item.name.includes("tour of"));
+    } else {
+        return data.filter((item) => item.name.includes("tour of"));
+    }
+
+  },
+};
